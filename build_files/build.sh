0ea3072c058d3f9e0886151f67f5cb7b2ec086b1
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2025-06-21 02:47:17 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,4 +2,7 @@
 
 set -ouex pipefail
 
-dnf5 update -y
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
+dnf5 -y update
+dnf5 -y install rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data
