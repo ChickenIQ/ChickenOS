18dd0c8a08d84468503de2a8af76a5c067076bc7
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2025-06-21 02:53:19 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,3 +6,6 @@ dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
 dnf5 -y update
 dnf5 -y install rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data
+
+flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-delete fedora
