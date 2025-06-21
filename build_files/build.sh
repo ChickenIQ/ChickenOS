377c4dd98a98af7d00305c44192bc4868294ca6b
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2025-06-21 03:06:04 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,5 +7,9 @@ dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
 dnf5 -y update
 dnf5 -y install rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data
 
+dnf5 -y remove firefox kwrite kfind kdebugsettings khelpcenter plasma-welcome krfb kde-connect kcharselect kjournald
+dnf5 -y autoremove 
+
+dnf5 -y install vim
+
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak remote-delete fedora
