e31b66fb73debf73dd7a717dfdbe22bd3833c80b
ChickenIQ <36515619+ChickenIQ@users.noreply.github.com>
2025-06-21 03:55:43 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,10 +4,8 @@ set -ouex pipefail
 
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y update
-dnf5 -y install rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data dnf-plugins-core dnf5-plugins
-dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
-
-dnf5 -y group install kde-desktop
+dnf5 -y install rpmfusion-free-appstream-data rpmfusion-nonfree-appstream-data
+dnf5 -y remove firefox kwrite kfind kdebugsettings khelpcenter plasma-welcome krfb kde-connect kcharselect kjournald
+dnf5 -y autoremove 
 dnf5 -y install vim
-
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
