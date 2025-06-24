3dd37207a0372b5f8f43e33547fbf7f39692719f
ChickenIQ <emibenzar@gmail.com>
2025-06-24 22:09:56 +0300

    fix

--- build_files/03-nvidia.sh
+++ build_files/03-nvidia.sh
@@ -2,7 +2,6 @@
 set -ouex pipefail
 [ "$VARIANT" != "nvidia" ] && exit 0
 
-
 echo 'kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1"]' > /usr/lib/bootc/kargs.d/00-nvidia.toml
 dnf5 config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 
