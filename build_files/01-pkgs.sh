f8ee527a0c4e90d6ff40bacff7936d315b13c462
ChickenIQ <emibenzar@gmail.com>
2025-06-26 00:24:41 +0300

    fix

--- build_files/01-pkgs.sh
+++ build_files/01-pkgs.sh
@@ -2,69 +2,41 @@
 set -ouex pipefail
 
 # Minimize System
-# rm -r /etc/skel/.mozilla
-# dnf5 -y remove \
-#   plasma-browser-integration \
-#   fedora-chromium-config-kde \
-#   fedora-chromium-config \
-#   fedora-flathub-remote \
-#   fedora-bookmarks \
-#   firewall-config \
-#   plasma-welcome \
-#   kdebugsettings \
-#   kinfocenter \
-#   khelpcenter \
-#   kcharselect \
-#   kde-connect \
-#   kjournald \
-#   toolbox \
-#   firefox \
-#   kwrite \
-#   kfind \
-#   krfb 
+rm -r /etc/skel/.mozilla
+dnf5 -y remove \
+  plasma-browser-integration \
+  fedora-chromium-config-kde \
+  fedora-chromium-config \
+  fedora-flathub-remote \
+  fedora-bookmarks \
+  firewall-config \
+  plasma-welcome \
+  kdebugsettings \
+  kinfocenter \
+  khelpcenter \
+  kcharselect \
+  kde-connect \
+  kjournald \
+  toolbox \
+  firefox \
+  kwrite \
+  kfind \
+  krfb 
 
 
 # Install Packages
-# dnf5 -y install \
-#   plasma-firewall \
-#   ananicy-cpp \
-#   scx-scheds \
-#   distrobox \
-#   fastfetch \
-#   starship \
-#   nvtop \
-#   kate \
-#   btop \
-#   fish \
-#   vim 
-
-
-# bootc-minimal-blus test
 dnf5 -y install \
-  attr \
-  bash-completion \
-  hostname \
-  iproute \
-  jq \
-  less \
-  vim-minimal \
-  podman skopeo \
-  crun criu criu-libs \
-  cryptsetup \
-  lvm2 \
-  tar \
-  zram-generator \
-  iptables-nft \
-  NetworkManager \
-  openssh-clients \
-  openssh-server \
-  linux-firmware \
-  polkit \
-  sudo \
-  tzdata \
-  rpm-ostree nss-altfiles \
-  fwupd 
-
+  plasma-firewall \
+  ananicy-cpp \
+  scx-scheds \
+  distrobox \
+  fastfetch \
+  starship \
+  nvtop \
+  kate \
+  btop \
+  fish \
+  vim 
 
 
 dnf5 -y install \
--- build_files/02-desktop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ouex pipefail
-
-dnf5 -y group install kde-desktop
