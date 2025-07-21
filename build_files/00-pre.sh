5bb2a6f9ab7cdf62cf87d253c854c7209c8a2a07
ChickenIQ <emibenzar@gmail.com>
2025-07-21 05:49:41 +0300

    lto on nvidia

--- build_files/00-pre.sh
+++ build_files/00-pre.sh
@@ -16,6 +16,7 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 dnf5 -y install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
 dnf5 -y copr enable alternateved/keyd
 dnf5 -y copr enable atim/starship
 dnf5 -y copr enable petersen/nix
--- build_files/02-kernel.sh
+++ build_files/02-kernel.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -ouex pipefail
 
-if [ "$VARIANT" != "nvidia" ]; then
-  dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
-  KERNEL_PKGS="kernel-cachyos-lto kernel-cachyos-lto-devel-matched"
-else
-  dnf5 -y copr enable bieszczaders/kernel-cachyos
-  KERNEL_PKGS="kernel-cachyos kernel-cachyos-devel-matched"
-fi
-
 
 # Setup Packages
-dnf5 -y install --allowerasing cachyos-settings scx-scheds sbsigntools 
+dnf5 -y install --allowerasing cachyos-settings scx-scheds sbsigntools openssl
 echo "SCX_SCHEDULER=scx_lavd" > /etc/default/scx && systemctl enable scx.service
 
 
 # Replace Kernel
-dnf5 -y install $KERNEL_PKGS 
+dnf5 -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 rpm --nodeps --erase kernel-{core,modules,modules-core,modules-extra}
 setsebool -P domain_kernel_load_modules on
