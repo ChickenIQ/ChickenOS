19c5b8ec4e01476cb2955d868a88b294ce474652
ChickenIQ <emibenzar@gmail.com>
2025-08-01 10:37:02 +0300

    revert kernel changes

--- build_files/02-kernel.sh
+++ build_files/02-kernel.sh
@@ -8,6 +8,6 @@ echo "SCX_SCHEDULER=scx_lavd" > /etc/default/scx && systemctl enable scx.service
 
 
 # Replace Kernel
-dnf5 -y install kernel-xanmod-edge kernel-xanmod-edge-devel-matched
+dnf5 -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
 rpm --nodeps --erase kernel-{core,modules,modules-core,modules-extra}
 setsebool -P domain_kernel_load_modules on
