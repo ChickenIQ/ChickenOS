b8c5dadc008d8510c74b451ba1149dcd2f8bb890
ChickenIQ <emibenzar@gmail.com>
2025-08-01 10:01:22 +0300

    xanmod

--- build_files/02-kernel.sh
+++ build_files/02-kernel.sh
@@ -3,11 +3,11 @@ set -ouex pipefail
 
 
 # Setup Packages
-dnf5 -y install --allowerasing cachyos-settings scx-scheds sbsigntools openssl
+dnf5 -y install --allowerasing scx-scheds sbsigntools openssl
 echo "SCX_SCHEDULER=scx_lavd" > /etc/default/scx && systemctl enable scx.service
 
 
 # Replace Kernel
-dnf5 -y install kernel-cachyos-lto kernel-cachyos-lto-devel-matched
+dnf5 -y install kernel-xanmod-edge kernel-xanmod-edge-devel-matched
 rpm --nodeps --erase kernel-{core,modules,modules-core,modules-extra}
 setsebool -P domain_kernel_load_modules on
--- build_files/03-nvidia.sh
+++ build_files/03-nvidia.sh
@@ -4,20 +4,19 @@ set -ouex pipefail
 
 
 # Install Driver
-dnf5 -y install kernel-cachyos-lto-nvidia-open xorg-x11-drv-nvidia-cuda
-# akmods --force --kernels $(basename -a /usr/src/kernels/*/)
-
+dnf5 -y install akmod-nvidia xorg-x11-drv-nvidia-cuda
+akmods --force --kernels $(basename -a /usr/src/kernels/*/)
 
 # Early Load NVIDIA Drivers
 sed -i "s@omit_drivers@force_drivers@g" /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 sed -i "s@ nvidia @ i915 amdgpu nvidia @g" /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 
 
-# # Setup Sysusers
-# cat > /usr/lib/sysusers.d/akmods.conf <<'EOF'
-# u akmods 967:966 "User is used by akmods to build akmod packages" /var/cache/akmods/ /sbin/nologin
-# g akmods 966
-# EOF
+# Setup Sysusers
+cat > /usr/lib/sysusers.d/akmods.conf <<'EOF'
+u akmods 967:966 "User is used by akmods to build akmod packages" /var/cache/akmods/ /sbin/nologin
+g akmods 966
+EOF
 
 
 # Setup Kargs
