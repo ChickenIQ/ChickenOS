bcb00fb8e2bcaf8513a669b1d45c4e97d9c22fa4
ChickenIQ <emibenzar@gmail.com>
2025-08-31 07:43:07 +0300

    fix build

--- build_files/03-nvidia.sh
+++ build_files/03-nvidia.sh
@@ -10,10 +10,15 @@ dnf5 -y install akmod-nvidia xorg-x11-drv-nvidia-cuda
 sed -i "s@omit_drivers@force_drivers@g" /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 sed -i "s@ nvidia @ i915 amdgpu nvidia @g" /usr/lib/dracut/dracut.conf.d/99-nvidia-dracut.conf
 
-
 # Setup Kargs
 cat > /usr/lib/bootc/kargs.d/00-nvidia.toml <<'EOF'
 kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1", "nvidia.NVreg_EnableGpuFirmware=0"]
+EOF
+
+# Setup Sysusers
+cat > /usr/lib/sysusers.d/akmods.conf <<'EOF'
+u akmods 967:966 "User is used by akmods to build akmod packages" /var/cache/akmods/ /sbin/nologin
+g akmods 966
 EOF
 
 # Force Build
